1b51da9b10a130591866dd88581d21fe158e724b
raihan <mahendraihan@gmail.com>
2020-09-14 20:01:22 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_mvc_cctvsnapshot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
